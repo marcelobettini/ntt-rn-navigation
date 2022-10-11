9f338ab2de995646e76cc5d2dc5cf9d49cc0e725
Marcelo_Bettini <marcelobettini@yahoo.com.ar>
2022-10-11 14:10:24 -0300

    initial set up

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+//TODO: https://www.npmjs.com/package/@react-navigation/native-stack
+
+import HomeScreen from './screens/HomeScreen';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator()
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* <Stack.Screen></Stack.Screen>
+        <Stack.Screen></Stack.Screen> */}
+      </Stack.Navigator>
+
+
+
+    </NavigationContainer>
+
   );
 }
 
