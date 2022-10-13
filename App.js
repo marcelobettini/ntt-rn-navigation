185bb27ef25a089f397c51c73274524e7c8e7bfb
Marcelo_Bettini <marcelobettini@yahoo.com.ar>
2022-10-13 14:22:53 -0300

    algunas props de las muchas que provee React Navigation

--- App.js
+++ App.js
@@ -2,7 +2,6 @@
 import HomeScreen from './screens/HomeScreen';
 import ConfigScreen from './screens/ConfigScreen';
 import ProfileScreen from './screens/ProfileScreen';
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator()
@@ -11,9 +10,19 @@ const Stack = createNativeStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name='Config' component={ConfigScreen} />
+      <Stack.Navigator initialRouteName='Home' screenOptions={{
+        headerStyle: {
+          backgroundColor: 'lightgreen'
+        },
+        headerTintColor: 'darkgray', //esto solo para que recuerden la prop
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 30,
+        }
+      }}>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "it begins here" }} />
+        <Stack.Screen name='Config' component={ConfigScreen} options={{ title: "configurate algo, cat 😎" }} />
         <Stack.Screen name='Profile' component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
